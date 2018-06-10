52ab42477a5dec139d2ace885544193174b6e1c5
Maicon Silva <maiconrs95@gmail.com>
2018-06-09 21:29:11 -0300

    update SISAPS
    update SISAPS

--- src/includes/novoPlanta.php
+++ src/includes/novoPlanta.php
@@ -1,5 +1,7 @@
 <?php
 
+    //header("Content-type: application/json; charset=utf-8");
+
     session_start();
     require_once('conexao.php');
 
@@ -14,13 +16,15 @@
     $modo_preparo = (isset($_POST['modo_preparo'])) ? $_POST['modo_preparo'] : '' ;  
     $bibliografia = (isset($_POST['bibliografia'])) ? $_POST['bibliografia'] : '' ;   
     $sintomas = (isset($_POST['duallistbox_demo2'])) ? $_POST['duallistbox_demo2'] : '' ;  
-    $associar = array_chunk($sintomas, count($sintomas) / 2);
+    
+    //var $associar = array_chunk($associar, ceil(count($associar) / 2));
+
     $conexao = new db();
     $link = $conexao->conn_mysql();
 
-    if(!is_uploaded_file($_FILES['arquivo']['tmp_name'])){
+    if(!is_uploaded_file($_FILES['arquivo']['tmp_name']) || ($_FILES["file"]["type"] != "image/jpg") || ($_FILES["file"]["type"] != "image/png") || ($_FILES["file"]["type"] != "image/jpeg")){
         
-        $targetPath = "";
+        $targetPath = "img/sem-foto.jpg";
 
     }else{
 
@@ -30,9 +34,13 @@
     }
 
     $sql = "INSERT INTO tb_plantas (nome_cientifico, nome_popular, modo_preparo, foto_planta, cuidados, efeitos_colaterais, principio_efeitos, bibliografia, id_parte_planta, id_user, estado, cidade, regiao, pais, id_status, comentarios) VALUES
-    ('$nome_c', '$nome_p', '$modo_preparo', '$targetPath', '$cuidados', '$efeitos','$principio_ativo', '$bibliografia','$sintomas', '$id_usuario', 'estado', 'cidade','$regiao','pais', 1, 'Planta Pendente de Verificação')";
+    ('$nome_c', '$nome_p', '$modo_preparo', '$targetPath', '$cuidados', '$efeitos','$principio_ativo', '$bibliografia','$parte_planta', '$id_usuario', 'estado', 'cidade','$regiao','pais', 1, 'Planta Pendente de Verificacao')";
+
+    $procedure_planta = "call cadastro_planta('$nome_c','$nome_p','$modo_preparo','$targetPath','$cuidados','$efeitos','$principio_ativo','$bibliografia', '$parte_planta','$id_usuario','$regiao')";
+
+    $result_id = mysqli_query($link, $procedure_planta);
     
-    $retorno = array('DADOS' => $sql, 'Sintomas' => $associar[0]);
-    echo json_encode($retorno);
+    $retorno = array('DADOS' => $sql, 'Sintomas' => $sintomas);
+    echo json_encode($procedure_planta);
     exit();
 ?>
