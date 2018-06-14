6188de60c7224b9a5ecb9a132a331c5df3fd2b15
Maicon R Silva <maiconrs95@gmail.com>
2018-06-14 11:31:05 -0300

    update SISAPS
    update SISAPS

--- src/includes/getPlantasProfessor.php
+++ src/includes/getPlantasProfessor.php
@@ -3,7 +3,7 @@
 header("Content-Type:" .  "application/json");
 require_once('conexao.php');
 
-$sql = "SELECT  tp.id_plantas, Descricao, nome_cientifico, nome_user FROM tb_plantas as tp JOIN tb_user as tu ON tu.id_user = tp.id_user JOIN tb_status ts ON ts.id_status = tp.id_status
+$sql = "SELECT tp.id_plantas, Descricao, nome_cientifico, nome_user FROM tb_plantas as tp JOIN tb_user as tu ON tu.id_user = tp.id_user JOIN tb_status ts ON ts.id_status = tp.id_status
 Where tp.id_status = 1";
 
 $conexao = new db();
