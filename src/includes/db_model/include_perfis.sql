ed029cafcaf0d7afcd25cec266eb51b75ff457c5
Maicon R Silva <maiconrs95@gmail.com>
2018-06-18 16:33:59 -0300

    update SISAPS
    update SISAPS

--- src/includes/db_model/include_perfis.sql
+++
@@ -1,5 +0,0 @@
-insert into tb_perfil(perfil, observacao) values ('Administrador','O administrador tem para manter usuários, plantas e sintomas.');
-
-insert into tb_perfil(perfil, observacao) values ('Professor','O professor tem acessos para validar/alterar cadastros previamente feito pelo assistente. E assim coloca-los em produção.');
-
-insert into tb_perfil(perfil, observacao) values ('Assistente','O assistente irá realizar os cadastro de plantas, sintomas e seus derivados.');
