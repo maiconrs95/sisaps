8c5775ec1f680750bb6bebf2277467bf0fc54e78
Maicon R Silva <maiconrs95@gmail.com>
2018-06-18 18:08:32 -0300

    update SISAPS
    update SISAPS

--- src/js/controller.js
+++ src/js/controller.js
@@ -94,10 +94,10 @@ function controlaView(conteudo) {
     //alterar planta
     else if (conteudo == 'view/alterar_planta.html') {
 
-        setInterval(function(){ consultaplantas(); }, 300000);
-        setInterval(function(){ getPartePlanta(); }, 300000);
-        
+        consultaplantas();
         //obtemSintomas();
+        getPartePlanta();
+
         $('.close').click(function (){
             $('#previewing').attr('src', 'img/plantas/sem-foto.jpg');
             $("input:text").val("");
@@ -146,7 +146,7 @@ function controlaView(conteudo) {
     //alterar sintoma
     else if (conteudo == 'view/alterar_sintoma.html') {
 
-        setInterval(function(){ consultaSintomas(); }, 300000);       
+        consultaSintomas();
 
         $('.close').click(function () {
             $('.alert-msg').hide();
@@ -178,8 +178,8 @@ function controlaView(conteudo) {
         $('.msgn-aprovado-sintoma').hide();
         $('.alert-mgn-sintoma').hide();
 
-        setInterval(function(){ consultaplantasPendente(); }, 300000);
-        setInterval(function(){ consultasintomasPendente(); }, 300000);
+        consultaplantasPendente();
+        consultasintomasPendente();
 
         $("#find-plantap").on("keyup", function () {
             var value = $(this).val().toLowerCase();
@@ -249,9 +249,9 @@ function controlaView(conteudo) {
         $('.sintomas-pendentes').hide();
         $('.plantas-pendentes').hide();
 
-        setInterval(function(){ consultaPlantasPendentes(); }, 300000);
-        setInterval(function(){ consultaSintomasPendentes(); }, 300000);
-        setInterval(function(){ getPartePlanta(); }, 300000);
+        consultaPlantasPendentes();
+        consultaSintomasPendentes();
+        getPartePlanta();
         
         $('.close').click(function () {
             $('.alert-msg').hide();
